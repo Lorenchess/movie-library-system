a6362768fc3a336b9561825df667343da2fa9745
Ramon Lorente <ramon_lorente@yahoo.com>
2024-06-03 06:47:13 -0400

    updating security filter chain and configuration for connecting with ui

--- src/main/java/com/chess4math/movie_library_system/config/SecurityConfig.java
+++ src/main/java/com/chess4math/movie_library_system/config/SecurityConfig.java
@@ -1,8 +1,30 @@
 package com.chess4math.movie_library_system.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) //For H2 console
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/", "/login**", "/h2-console/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauthLogin -> oauthLogin.defaultSuccessUrl("/api/welcome", true)
+
+                ).oauth2ResourceServer(oauthResourceServer -> oauthResourceServer.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
 
 }
